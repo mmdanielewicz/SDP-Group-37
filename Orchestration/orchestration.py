d58fbaa88d9a2398d403821cc68aa80f3dc04af6
systembrace <connor.cybart@uconn.edu>
2025-11-07 17:58:52 -0500

    Custom arguments for quick testing

--- Orchestration/orchestration.py
+++ Orchestration/orchestration.py
@@ -1,16 +1,21 @@
+import sys
 from ollama import chat
 from ollama import ChatResponse
 
-def get_response(prompt):
+
+def get_response(prompt, model="llama3.1:8b"):
     response: ChatResponse = chat(
-        model="llama3.1:8b", 
+        model=model, 
         messages=[{'role': 'user', 'content': prompt}]
     )
     return response.message.content
-    
+
 def test():
-    print(get_response("In a short sentence, tell me what color the sky usually is."))
-    
+    prompt="In a short sentence, tell me what color the sky usually is."
+    if len(sys.argv)==2:
+        prompt=sys.argv[1]
+    print("Prompt:",prompt)
+    print("Response:",get_response(prompt))
 
 if __name__=="__main__":
     test()
